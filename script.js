c2c2b5d3b861fa7dc6557a6dd08cedae55f790d6
Maycon Mota <mayyccon21@hotmail.com>
2021-09-25 10:59:42 -0300

    att script

---
+++ script.js
@@ -0,0 +1,66 @@
+function isValidCPF(cpf) { 
+	console.log(cpf.length);
+
+
+	if (
+    	cpf.length != 11 
+    	
+	) {
+    	return false;
+	} else {
+    	var numeros = cpf.substring(0, 9);
+    	var digitos = cpf.substring(9);
+
+    	var soma = 0;
+    	for (var i = 10; i > 1; i--) {
+        	soma += numeros.charAt(10 - i) * i;
+    	}
+    	console.log(soma);
+    	//se a operacao for igual a 0. % eh mod (resto)
+    	var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11); // capturando o resto da variavel soma
+    	//validacao do primeiro digito
+    	if (resultado != digitos.charAt(0)) {
+        	return false;
+    	}
+    	soma = 0; // soma recebeu zero
+    	numeros = cpf.substring(0, 10);
+
+    	for (var k = 11; k > 1; k--) {
+        	soma += numeros.charAt(11 - k) * k;
+    	}
+    	resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    	//validacao do segundo digito
+    	if (resultado != digitos.charAt(1)) {
+        	return false;
+    	}
+
+    	return true;
+
+	}
+}
+
+function validacao() {
+	console.log("Funcao de validacao")
+
+	var cpf = document.getElementById('cpf_digitado').value;
+
+	var resultadoValidacao = isValidCPF(cpf) // essa variavel recebe o cpf digitado evalidado na função acima //
+
+	if (resultadoValidacao)   { // se o cpf for verdadeiro ele mostra o emoji verde //
+    	var element = document.getElementById('result');
+    	element.innerHTML = `\u{2705}`
+	} else { // se for invalido ele mostra emoji vermelho //
+    	var element = document.getElementById('result');
+    	element.innerHTML = `\u{274C}`
+
+        // abaixo é o alerta com tempo de 1 seg apos aparecer o emoji invalido // 
+
+		  setTimeout( () =>
+          { alert("CPF Invalido, confira o numero digitado!"); }, 1000); //  o 1000 milisegundos significa o tempo para executar o alert //
+	}
+
+}
+
+function enviar() {
+    alert("Formulario Enviado com Sucesso!")
+}
